6dc117c1776d6f7db3a99ff0f5a95efa61ff4d3b
kramlj00 <kramlj00@fesb.hr>
2022-08-16 12:49:56 +0200

    Replaced img with next/image

--- components/TransitionedCardFan/index.jsx
+++ components/TransitionedCardFan/index.jsx
@@ -1,24 +1,34 @@
+import Image from "next/image";
 import styles from "./cardFan.module.scss";
 
 const TransitionedCardFan = () => {
   return (
     <div>
       <div className={styles.cardfan}>
-        <img
-          src="https://res.cloudinary.com/kristina1950/image/upload/v1659702042/rooms/room17-image_ir36t2.jpg"
-          alt="cardfan_pic3"
-          className={styles.pic3}
-        />
-        <img
-          src="https://res.cloudinary.com/kristina1950/image/upload/v1659702140/exterior/exterior15-image_unyat4.jpg"
-          alt="cardfan_pic2"
-          className={styles.pic2}
-        />
-        <img
-          src="https://res.cloudinary.com/kristina1950/image/upload/v1659702138/exterior/exterior18-image_fi613x.jpg"
-          alt="cardfan_pic1"
-          className={styles.pic1}
-        />
+        <div className={`${styles.cardfanImage} ${styles.pic3}`}>
+          <Image
+            src="https://res.cloudinary.com/kristina1950/image/upload/v1659702042/rooms/room17-image_ir36t2.jpg"
+            alt="cardfan_pic3"
+            width={500}
+            height={330}
+          />
+        </div>
+        <div className={`${styles.pic2} ${styles.cardfanImage}`}>
+          <Image
+            src="https://res.cloudinary.com/kristina1950/image/upload/v1659702140/exterior/exterior15-image_unyat4.jpg"
+            alt="cardfan_pic2"
+            width={500}
+            height={330}
+          />
+        </div>
+        <div className={`${styles.pic1} ${styles.cardfanImage}`}>
+          <Image
+            src="https://res.cloudinary.com/kristina1950/image/upload/v1659702138/exterior/exterior18-image_fi613x.jpg"
+            alt="cardfan_pic1"
+            width={500}
+            height={330}
+          />
+        </div>
       </div>
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
         <filter id="blur">
@@ -48,6 +58,6 @@ const TransitionedCardFan = () => {
       </svg>
     </div>
   );
-}
+};
 
 export default TransitionedCardFan;
